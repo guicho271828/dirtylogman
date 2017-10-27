33e06b7468dff766980c338e9dd13590ea0515dd
Masataro Asai <guicho2.71828@gmail.com>
2017-10-27 23:03:11 +0900

    record the results into .res (for checking the bug)

--- benchmark/benchmark.sh
+++ benchmark/benchmark.sh
@@ -5,7 +5,7 @@ test -d fig2-base || tar zxvf archive.tar.gz
 touch 0.log
 trap "rm 0.log" EXIT
 
-outfile=$((1 + $(basename $(ls *.log | sort -rh | head -n 1) .log))).log
+outfile=$((1 + $(basename $(ls *.log | sort -rh | head -n 1) .log)))
 
 # ( echo 1 ; echo 2 >&2 ) >/dev/null 2>&1
 # ( echo 1 ; echo 2 >&2 ) 2>&1 >/dev/null
@@ -13,8 +13,8 @@ outfile=$((1 + $(basename $(ls *.log | sort -rh | head -n 1) .log))).log
 
 
 ( find fig2-base/ipc2008-opt-master-ad1e-a333af-2016-05-29-14-55 -name "*.out" | \
-      time -v -o /dev/fd/2 ../roswell/dirty.ros -p -y sample.yaml 2>&1 >/dev/null ) |
-    tee $outfile
+      time -v -o /dev/fd/2 ../roswell/dirty.ros -p -y sample.yaml 2>&1 >$outfile.res ) |
+    tee $outfile.log
 
 # larger db
 # find fig2-base -name "*.out" | time -v ../roswell/dirty.ros -p -y sample.yaml
