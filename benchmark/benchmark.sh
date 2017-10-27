5d09cc03a5de69a582a9c47b78adff29886cbe9b
Masataro Asai <guicho2.71828@gmail.com>
2017-10-27 22:01:59 +0900

    temporarily disable the larger benchmarks

--- benchmark/benchmark.sh
+++ benchmark/benchmark.sh
@@ -2,4 +2,9 @@
 
 test -d fig2-base || tar zxvf archive.tar.gz
 
-find fig2-base -name "*.out" | time -v ../roswell/dirty.ros -p -y sample.yaml
+
+find fig2-base/ipc2008-opt-master-ad1e-a333af-2016-05-29-14-55 -name "*.out" | \
+    time -v ../roswell/dirty.ros -p -y sample.yaml
+
+# larger db
+# find fig2-base -name "*.out" | time -v ../roswell/dirty.ros -p -y sample.yaml
